c18c1b31a93021dbea39e921690824b1dfcc42ce
SahilTondale <sahiltondale@gmail.com>
2026-04-01 23:34:49 +0530

    a2

--- NutriScan_app/apps.py
+++ NutriScan_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class IngredientAnalysisAppConfig(AppConfig):
+class NutriScanAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'NutriScan_app'
